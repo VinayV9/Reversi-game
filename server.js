bb55e9b75a20652542a2b1db8cc7c5f63cb6de5c
VinayV9 <vinayveeren@gmail.com>
2017-11-18 09:40:05 +0530

    everything clear

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 var express=require('express')
 var app =express()
+var bodyParser=require('body-parser')
+var port =3000
+
+app.use(bodyParser.json())
+app.use('/', express.static(__dirname + '/public'));
+app.use('/',express.static(__dirname+'js'))
+app.listen(port, function(){
+    console.log("port on %d",port)
+})
