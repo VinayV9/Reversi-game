6be3dd89b9aaaeb5d9010941b384da79a4722790
VinayV9 <vinayveeren@gmail.com>
2017-11-18 10:07:32 +0530

    all files placed in public folder

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var port =3000
 
 app.use(bodyParser.json())
 app.use('/', express.static(__dirname + '/public'));
-app.use('/',express.static(__dirname+'js'))
+
 app.listen(port, function(){
     console.log("port on %d",port)
 })
