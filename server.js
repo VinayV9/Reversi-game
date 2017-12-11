72743bb11e8810273cedb560b4909c77068258ef
VinayV9 <vinayveeren@gmail.com>
2017-12-11 16:49:42 +0530

    minor changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app= express()
 var http=require('http').Server(app)
 var io = require('socket.io')(http)
 
-var port =3000
+var port =(process.env.PORT || 3000)
 
 app.use('/', express.static(__dirname + '/public'));
 
