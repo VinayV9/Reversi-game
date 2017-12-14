fcc9422c8e93c418097d9e03ff6df739ac15caf9
VinayV9 <vinayveeren@gmail.com>
2017-12-14 22:25:18 +0530

    created uniue game id and sent to server

--- public/ng/app.js
+++ public/ng/app.js
@@ -4,12 +4,22 @@ app.controller('gameJoinCtrl', function($scope) {
 });
 
 app.controller('gameCreateCtrl',function($scope, $http){
+    $scope.generateCode=false;
+    $scope.Code;
+    var code=[6];
      $scope.create= function(){
-         var code=[6];
          for(var i=6;i>=0;i--){
              // generate random numbers btw 0 and 9 (inclusive)
             code[i]=Math.floor((Math.random() * 10));
          }
-    
+         $scope.Code=code.join("");
+
+         $http.post('/start2Player', { code:code.join("")}).
+         then(function(response) {
+            console.log("posted successfully");
+        }).catch(function(response) {
+            console.error("error in posting");
+        });
+         $scope.generateCode=true;
      }
 });
--- server.js
+++ server.js
@@ -2,16 +2,25 @@ var express=require('express')
 var app= express()
 var http=require('http').Server(app)
 var io = require('socket.io')(http)
-
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 var port =(process.env.PORT || 3000)
 
-app.use('/', express.static(__dirname + '/public'));
+app.use('/', express.static(__dirname + '/public'))
 
 io.on('connection', function(socket){
     console.log(socket.id)
     socket.on('updateBoard', function(balls){
        io.emit('updateBoard', balls)
     })
+})
+
+var gameIds=[]
+app.post('/start2Player', function(req, res){
+    console.log(req.body)
+     gameIds.push(req.body.code)
+     res.sendStatus(200)
 })
 
  http.listen(port, function(){
